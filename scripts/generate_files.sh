3551e0d5b34b96d3e0fdb0fec6c531c7b4e925cd
Morhov Denis <acikers@gmail.com>
2014-06-30 10:51:27 +0400

    script/generate_files.sh: ehm... fix for previous fix

--- scripts/generate_files.sh
+++ scripts/generate_files.sh
@@ -14,9 +14,10 @@ while [[ $counter -le $no_of_files ]];
 do
 	echo Creating file \#$counter;
 	let size=0;
-	while [[ $size -le $min_size ]]; do
-		let size=$(($size + $RANDOM % ($max_size - $min_size) + $min_size));
+	while [[ $size -le $max_size ]]; do
+		let size=$(($size*65536+$RANDOM));
 	done
+	let size=$(($size % ($max_size - $min_size) + $min_size));
 	dd bs=1 count=$size if=/dev/urandom of=./random_file.$counter;
 	let counter+=1;
 done
